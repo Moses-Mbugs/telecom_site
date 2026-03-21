d7a44f79d3e427fd8b77aef1a7e4ac9aa729978c
Moses Mbugua <126574366+Moses-Mbugs@users.noreply.github.com>
2026-03-21 13:01:10 +0300

    production build

--- telecom-backend/src/api/shop-page/controllers/shop-page.ts
+++
@@ -1,7 +0,0 @@
-/**
- * shop-page controller
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreController('api::shop-page.shop-page');
--- telecom-backend/src/api/shop-page/routes/shop-page.ts
+++
@@ -1,7 +0,0 @@
-/**
- * shop-page router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::shop-page.shop-page');
